9f7346e04a16a6d978a0a256349a6fb46be92a3a
Yura Savin <yurasavin@gmail.com>
2023-06-03 16:52:53 +0000

    Added handling of element width to hide titles

--- components/tickets/TicketFilters/TicketFilters.tsx
+++ components/tickets/TicketFilters/TicketFilters.tsx
@@ -1,28 +1,40 @@
 import FilterItem from "@components/shared/Filters/FilterItem/FilterItem";
 import Filters from "@components/shared/Filters/Filters";
+import { useSize } from "ahooks";
+import { useRef } from "react";
 import DateFilter from "./DateFilter/DateFilter";
 import LimitSelect from "./LimitSelect";
 import StatusSelect from "./StatusSelect/StatusSelect";
 import TenderTypeSelect from "./TenderTypeSelect/TenderTypeSelect";
 import TicketsSearch from "./TicketsSearch/TicketsSearch";
 
+const MINIMUM_TITLES_DISPLAY_WIDTH = 1410;
+
 const TicketFilters: React.FC = () => {
+  // TODO: Fix jumping of this element on sider collapse
+  const elementRef = useRef(null);
+  const size = useSize(elementRef);
+
+  const showTitle = !size || size.width >= MINIMUM_TITLES_DISPLAY_WIDTH;
+
   return (
-    <Filters>
-      <TicketsSearch />
-      <FilterItem title="Лимиты">
-        <LimitSelect />
-      </FilterItem>
-      <FilterItem title="Статус">
-        <StatusSelect />
-      </FilterItem>
-      <FilterItem title="Способ закупки">
-        <TenderTypeSelect />
-      </FilterItem>
-      <FilterItem title="Дата">
-        <DateFilter />
-      </FilterItem>
-    </Filters>
+    <div ref={elementRef}>
+      <Filters>
+        <TicketsSearch />
+        <FilterItem title={showTitle ? "Лимиты" : ""}>
+          <LimitSelect />
+        </FilterItem>
+        <FilterItem title={showTitle ? "Статус" : ""}>
+          <StatusSelect />
+        </FilterItem>
+        <FilterItem title={showTitle ? "Способ закупки" : ""}>
+          <TenderTypeSelect />
+        </FilterItem>
+        <FilterItem title={showTitle ? "Дата" : ""}>
+          <DateFilter />
+        </FilterItem>
+      </Filters>
+    </div>
   );
 };
 

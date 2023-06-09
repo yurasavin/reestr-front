b606d451671d3084c492a5ef7b2ca7d28a303a22
Yura Savin <yurasavin@gmail.com>
2023-06-09 15:25:26 +0000

    Refactored login form

--- components/login/Login/Login.tsx
+++ components/login/Login/Login.tsx
@@ -1,13 +1,12 @@
-import { LoadingOutlined } from "@ant-design/icons";
 import { useRequest } from "ahooks";
-import { Button, Checkbox, Form, message, Spin } from "antd";
+import { Button, Form, message } from "antd";
 import { useContext } from "react";
 import { useSWRConfig } from "swr";
 import styles from "./Login.module.css";
 
+import BluredLayout from "@components/shared/BluredLayout/BluredLayout";
 import { UserContext } from "@contexts/UserContext";
 import useHeaders from "@hooks/apis/resources/useHeaders";
-import { UserResponse } from "@hooks/apis/useUser";
 import { ErrorResponse, fetcher, Response } from "@services/api";
 import FormPassword from "../FormPassword";
 import FormUsername from "../FormUsername";
@@ -15,25 +14,33 @@ import FormUsername from "../FormUsername";
 interface LoginFormData {
   username: string;
   password: string;
-  remember: boolean;
 }
 
 interface LoginResponse {
-  token: string;
-  user: UserResponse;
+  access_token: string;
+  expires_in: number;
+  refresh_token: string;
+  scope: string;
+  token_type: string;
 }
 
 const Login: React.FC = () => {
-  const { user, setUser, isMutating, setAuthToken } = useContext(UserContext);
+  const { setAccessToken, setRefreshToken } = useContext(UserContext);
   const { mutate } = useSWRConfig();
   const headers = useHeaders();
 
   const onFinish = async (formValues: LoginFormData) => {
+    const requestData = {
+      client_id: process.env.NEXT_PUBLIC_AUTH_CLIENT_ID,
+      grant_type: "password",
+      ...formValues,
+    };
+
     const response: Response<LoginResponse> = await fetcher({
-      path: "auth/login/",
+      path: "oauth/token/",
       fetchParams: {
         method: "POST",
-        body: JSON.stringify(formValues),
+        body: JSON.stringify(requestData),
         headers,
       },
     });
@@ -41,11 +48,9 @@ const Login: React.FC = () => {
   };
 
   const onSuccess = ({ data }: Response<LoginResponse>) => {
-    if (data.token) {
-      setAuthToken(data.token);
-      setUser(data.user);
-      mutate(() => true, undefined, { revalidate: false });
-    }
+    setAccessToken(data.access_token);
+    setRefreshToken(data.refresh_token);
+    mutate(() => true, undefined, { revalidate: false });
   };
 
   const onError = (error: Error | ErrorResponse) => {
@@ -63,59 +68,29 @@ const Login: React.FC = () => {
     onError,
   });
 
-  if (isMutating) {
-    return (
-      <div className={styles["container-outer"]}>
-        <div className={styles["container-inner"]}>
-          Загрузка...
-          <Spin
-            indicator={<LoadingOutlined className={styles.spinner} spin />}
-          />
-        </div>
-      </div>
-    );
-  } else if (user) {
-    return null;
-  }
-
   return (
-    <div className={styles["container-outer"]}>
-      <div className={styles["container-inner"]}>
-        <Form
-          name="login-form"
-          className={styles.form}
-          requiredMark={false}
-          initialValues={{
-            remember: true,
-          }}
-          onFinish={run}
-          labelCol={{ span: 5 }}
-        >
-          <FormUsername />
-          <FormPassword />
-          <Form.Item>
-            <Form.Item name="remember" valuePropName="checked" noStyle>
-              <Checkbox>Запомнить меня</Checkbox>
-            </Form.Item>
-
-            <a className={styles.forgot} href="">
-              Восстановить пароль
-            </a>
-          </Form.Item>
-
-          <Form.Item>
-            <Button
-              type="primary"
-              htmlType="submit"
-              loading={loading}
-              className={styles["login-button"]}
-            >
-              Войти
-            </Button>
-          </Form.Item>
-        </Form>
-      </div>
-    </div>
+    <BluredLayout>
+      <Form
+        name="login-form"
+        className={styles.form}
+        requiredMark={false}
+        onFinish={run}
+        labelCol={{ span: 5 }}
+      >
+        <FormUsername />
+        <FormPassword />
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            loading={loading}
+            className={styles["login-button"]}
+          >
+            Войти
+          </Button>
+        </Form.Item>
+      </Form>
+    </BluredLayout>
   );
 };
 
